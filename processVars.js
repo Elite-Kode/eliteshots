0d8fd140c0b51c760a4f97515d687657d4d42100
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2020-02-07 16:11:16 +0530

    Updated image urls for dev

--- processVars.js
+++ processVars.js
@@ -1,17 +1,20 @@
-let host = '';
-let protocol = '';
+let host = ''
+let protocol = ''
+let imageUrlRoute = ''
 if (process.env.NODE_ENV === 'development') {
-  host = 'localhost:3004';
-  protocol = 'http';
+  host = 'localhost:3004'
+  protocol = 'http'
+  imageUrlRoute = 'https://cdn.eliteshots.gallery/file/eliteshots-dev/'
 } else if (process.env.NODE_ENV === 'production') {
-  host = 'eliteshots.gallery';
-  protocol = 'https';
+  host = 'eliteshots.gallery'
+  protocol = 'https'
+  imageUrlRoute = 'https://cdn.eliteshots.gallery/file/eliteshots/'
 }
 
-let version = require('./version');
+let version = require('./version')
 
 let defaultAlbumTitle = 'Default'
 
 module.exports = {
-  host, protocol, version, defaultAlbumTitle
+  host, protocol, imageUrlRoute, version, defaultAlbumTitle
 }
