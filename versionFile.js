fb9e6b35d2f2376d904322c499921490305b2bd6
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2021-03-27 21:34:36 +0530

    Project moved to subfolder as a preparation for modularisation

--- versionFile.js
+++ versionFile.js
@@ -1,12 +1,15 @@
 const path = require('path');
-const fs = require('fs-extra');
+const fs = require('fs');
+
 const appVersion = require('./package.json').version;
 
-const vueVersionFilePath = path.join(__dirname + '/src/version.js');
-const nodeVersionFilePath = path.join(__dirname + '/version.js');
+const frontendVersionFilePath = path.join(__dirname + '/frontend/src/version.js');
+const frontendNodeVersionFilePath = path.join(__dirname + '/frontend/version.js');
+const guildBotVersionFilePath = path.join(__dirname + '/guild_bot/version.js');
 
 const vueSrc = `export default '${appVersion}'\n`;
 const nodeSrc = `module.exports = '${appVersion}';\n`;
 
-fs.writeFileSync(vueVersionFilePath, vueSrc);
-fs.writeFileSync(nodeVersionFilePath, nodeSrc);
+fs.writeFileSync(frontendVersionFilePath, vueSrc);
+fs.writeFileSync(frontendNodeVersionFilePath, nodeSrc);
+fs.writeFileSync(guildBotVersionFilePath, nodeSrc);
