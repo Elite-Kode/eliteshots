34bfd0f1cc06e320119778100793d69f8aa13882
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2021-04-03 18:41:32 +0530

    Fix for not posting an uploaded image to discord when it is not used

--- backend/routes/self.js
+++ backend/routes/self.js
@@ -150,9 +150,11 @@ router.post('/upload', upload.single('screenshot'), async (req, res, next) => {
         await imageDocument.save()
         let formData = new FormData()
         formData.append('file', req.file.buffer, { filename: req.file.originalname })
-        await axios.post(`${secrets.companion_bot_endpoint}/moderation/new-upload`, formData, {
-          headers: formData.getHeaders()
-        })
+        if (secrets.discord_use) {
+          await axios.post(`${secrets.companion_bot_endpoint}/moderation/new-upload`, formData, {
+            headers: formData.getHeaders()
+          })
+        }
         res.status(201).send({})
       } else {
         res.status(403).send({})
