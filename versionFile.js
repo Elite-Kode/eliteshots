ab61270f4299f36cc13bf5e7dc6442afbbbc68b5
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2021-05-01 18:26:10 +0530

    Bumped version file

--- versionFile.js
+++ versionFile.js
@@ -3,13 +3,12 @@ const fs = require('fs');
 
 const appVersion = require('./package.json').version;
 
-const frontendVersionFilePath = path.join(__dirname + '/frontend/src/version.js');
-const frontendNodeVersionFilePath = path.join(__dirname + '/frontend/version.js');
+const backendVersionFilePath = path.join(__dirname + '/backend/version.js');
+const frontendVersionFilePath = path.join(__dirname + '/frontend/version.js');
 const guildBotVersionFilePath = path.join(__dirname + '/guild_bot/version.js');
 
-const vueSrc = `export default '${appVersion}'\n`;
-const nodeSrc = `module.exports = '${appVersion}';\n`;
+const nodeSrc = `module.exports = '${appVersion}'\n`;
 
-fs.writeFileSync(frontendVersionFilePath, vueSrc);
-fs.writeFileSync(frontendNodeVersionFilePath, nodeSrc);
+fs.writeFileSync(backendVersionFilePath, nodeSrc);
+fs.writeFileSync(frontendVersionFilePath, nodeSrc);
 fs.writeFileSync(guildBotVersionFilePath, nodeSrc);
